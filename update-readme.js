3d25f91fe9e3a93f8bd71f6ea50d9740b7d2edcb
Marco Hinz <dr.marco.buschmann@proton.me>
2026-01-08 11:53:06 +0100

    Aktualisieren von update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -6,6 +6,11 @@ const readmePath = path.join(__dirname, 'README.md');
 let readmeContent = fs.readFileSync(readmePath, 'utf-8');
 
 // Ersetze Platzhalter durch GitHub-Benutzername und Repository-Besitzer
+if (!githubUser || !githubOwner) {
+  console.error('Environment variables GITHUB_USER and GITHUB_OWNER are required.');
+  process.exit(1); // Exit with a failure code
+}
+
 const githubUser = process.env.GITHUB_USER;
 const githubOwner = process.env.GITHUB_OWNER;
 
