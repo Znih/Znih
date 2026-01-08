b1d0ed1ae399efe516b5bd657a20ca6fea1622f8
Marco Hinz <dr.marco.buschmann@proton.me>
2026-01-08 18:38:29 +0100

    Aktualisieren von update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
+// Backup4read
+const backupPath = `${readmePath}.backup`;
+fs.copyFileSync(readmePath, backupPath);
+console.log(`Backup created at ${backupPath}`);
+
 // Lese die aktuelle README.md
 const readmePath = path.join(__dirname, 'README.md');
 let readmeContent = fs.readFileSync(readmePath, 'utf-8');
@@ -12,12 +17,15 @@ const githubOwner = process.env.GITHUB_OWNER;
 if (!githubUser || !githubOwner) {
   console.error('Environment variables GITHUB_USER and GITHUB_OWNER are required.');
   process.exit(1); // Exit with a failure code
-} {console.log('Successfully updated README.md with GitHub user and owner information.');
 }
+
+console.log('Successfully updated README.md with GitHub user and owner information.');
 
 readmeContent = readmeContent.replace(/{{GITHUB_USER}}/g, githubUser);
 readmeContent = readmeContent.replace(/{{GITHUB_OWNER}}/g, githubOwner);
 
 // Schreibe die aktualisierte README.md zurück
 fs.writeFileSync(readmePath, readmeContent);
+
+
 
